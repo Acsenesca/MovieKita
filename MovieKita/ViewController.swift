e710918e940b4a72dcab0f58fc96a197cbb64957
Stevanus Prasetyo Soemadi <stevanus.pras@gmail.com>
2020-07-01 19:51:40 +0700

    Installing carthage and adding frameworks

---
+++ MovieKita/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieKita
+//
+//  Created by Stevanus Prasetyo Soemadi on 01/07/20.
+//  Copyright © 2020 Stevanus Prasetyo Soemadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
